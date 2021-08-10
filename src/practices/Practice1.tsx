fbc717c839fec4dc0fa9beabcd19d1cde49112ef
sccala <sccala18@gmail.com>
2021-08-10 14:15:50 -0700

    inital setup

---
+++ src/practices/Practice1.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react'
+
+export const Practice1 = () => {
+    return
+    
+        <div>
+        <p>練習問題</p>
+        </div>
+        
+    
+}
---
+++ src/typeList.tsx
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+
+let bool: boolean = true
+
+let num: number = 0
+
+let str: string = 'A'
+
+let arr1: Array<number> = [0, 1, 2]
+
+let arr2: number[] = [0, 1, 2]
